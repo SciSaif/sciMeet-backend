fc766a7a62f3ff0003ed1887213757f479eea4ed
saif <saiflll284@gmail.com>
2023-04-30 23:32:54 +0530

    serviceAccount.json shifted to env variable

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -1,8 +1,15 @@
 import admin from "firebase-admin";
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-const serviceAccount = require("./serviceAccount.json");
+// const serviceAccount = require("./serviceAccount.json");
+import dotenv from "dotenv";
+dotenv.config();
 
+const serviceAccount = JSON.parse(
+    process.env.FIREBASE_SERVICE_ACCOUNT as string
+);
+// console.log("serviceAccount", process.env.FIREBASE_SERVICE_ACCOUNT);
+console.log("serviceAccount", serviceAccount);
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
